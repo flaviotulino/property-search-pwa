1ac5a8a6197297ce2ae4b908d556808a0180800d
Flavio Tulino <flaviotulino@MacBookPro-2.broadband>
2026-04-22 13:44:02 +0100

    readded badge

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+import { useRegisterSW } from "virtual:pwa-register/vue";
+
+const { needRefresh, updateServiceWorker } = useRegisterSW();
+
+const refreshApp = () => {
+  updateServiceWorker(true); // This reloads the page and updates the PWA
+};
+</script>
 
 <template>
-  <v-ons-page>
+  <div
+    v-if="needRefresh"
+    class="fixed inset-0 bg-gray-900 bg-opacity-75 flex flex-col items-center justify-center space-y-4 z-50"
+  >
+    <span>New content available, click on reload button to update.</span>
+    <button @click="refreshApp">Reload</button>
+  </div>
+
+  <v-ons-page v-else>
     <RouterView :key="$route.fullPath" />
   </v-ons-page>
 </template>
